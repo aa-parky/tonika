47bedf83a1180e68891f0ee14d7e3b930106a856
aapark <aa.parky@gmail.com>
2025-09-14 21:33:00 +0100

    setting up stylelint and BEM rulebook

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -66,7 +66,7 @@ module.exports = {
                     // (Disallows .block--modifier)
                     // Where element/modifier are lowercase letters, digits, or hyphens.
                     return new RegExp(
-                        `^\\.${block}(?:__(?:[a-z0-9-]+)(?:--[a-z0-9-]+)?)?$`
+                        `^\\.${block}(?:__[a-z0-9-]+(?:--[a-z0-9-]+)?)?$`
                     );
                 },
             },
@@ -84,7 +84,7 @@ module.exports = {
             //  - block__element--modifier
             // Disallow:
             //  - block--modifier (must have "__" before "--")
-            "^(?:(?!.*--)[a-z][a-z0-9-]*|[a-z][a-z0-9-]*__(?:[a-z0-9-]+)(?:--[a-z0-9-]+)?)$",
+            "^(?:(?!.*--)[a-z][a-z0-9-]*|[a-z][a-z0-9-]*__[a-z0-9-]+(?:--[a-z0-9-]+)?)$",
             {
                 // Consider nested selectors when validating
                 resolveNestedSelectors: true,
